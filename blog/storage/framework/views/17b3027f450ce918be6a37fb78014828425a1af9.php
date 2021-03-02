9767087b5b04efa9c15b4a922b159beee450f4b5
emrizki <emrizkidev@gmail.com>
2021-03-02 23:19:00 +0700

    membuat web statis dengan laravel

---
+++ blog/storage/framework/views/17b3027f450ce918be6a37fb78014828425a1af9.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Register</title>
+</head>
+<body>
+  <h1>Buat Account Baru</h1>
+  <h2>Sign Up Form</h2>
+  <form action="/register" method="POST">
+    <?php echo csrf_field(); ?>
+    <!-- First Name -->
+    <label for="firts-name">First Name: </label><br>
+    <input type="text" name="first_name"><br><br>
+
+    <!-- Last Name -->
+    <label for="last-name">Last Name: </label><br>
+    <input type="text" name="last_name"><br><br>
+
+    <!-- Gender -->
+    <label>Gender:</label><br>
+    <input type="radio" name="gender" value="man">Man<br>
+    <input type="radio" name="gender" value="woman">Woman<br>
+    <input type="radio" name="gender" value="other">Other<br><br>
+
+    <!-- Nationality -->
+    <label for="nationality">Nationality: </label>
+    <select name="nationality" id="nationality">
+      <option value="indonesian">Indonesian</option>
+      <option value="american">American</option>
+      <option value="arabian">Arabian</option>
+    </select><br><br>
+
+    <!-- Language Spoken -->
+    <label>Language Spoken: </label><br>
+    <input type="checkbox" name="language" value="bahasa_indonesia">Bahasa Indonesia<br>
+    <input type="checkbox" name="language" value="english">English<br>
+    <input type="checkbox" name="language" value="arabic">Arabic<br>
+    <input type="checkbox" name="language" value="japanese">Japanese<br><br>
+
+    <!-- Bio -->
+    <label for="bio">Bio: </label><br>
+    <textarea name="bio" id="" cols="30" rows="10"></textarea><br><br>
+    <button type="submit">Sign Up</button>
+  </form>
+</body>
+</html><?php /**PATH /home/rizki/Documents/laravel-webdev-sanbarcode/Week 3/Day 2 - Membuat Web Statis dengan Laravel/blog/resources/views/register.blade.php ENDPATH**/ ?>
---
+++ blog/storage/framework/views/7b0e2038fac68178f0fabf58869ea71dd5d77f07.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Selamat Datang <?php echo e($first_name); ?> <?php echo e($last_name); ?> </h1>
+    <h3>Terima kasih telah bergabung di Sanberbook. Social Media kita bersama!</h3>
+</body>
+</html><?php /**PATH /home/rizki/Documents/laravel-webdev-sanbarcode/Week 3/Day 2 - Membuat Web Statis dengan Laravel/blog/resources/views/welcome.blade.php ENDPATH**/ ?>
